0f70a50c648ca054b8e6ceff00314f20aa7449a5
Niv Shirazi <nivshir@post.bgu.ac.il>
2023-08-06 21:59:10 +0300

    update screens

--- src/pages/home/homePage.tsx
+++ src/pages/home/homePage.tsx
@@ -5,10 +5,13 @@ import Button from '../../shared/button/button';
 import InputFile from '../../shared/inputFile/inputFile';
 import './homePage.css';
 import { useNavigate } from 'react-router';
+import vision from '@google-cloud/vision';
+
 
 const HomePage = () => {
 
   const [file, setFile] = useState<File>();
+  const [loading, setLoading] = useState<boolean>(false);
 
   const navigate = useNavigate();
 
@@ -16,18 +19,26 @@ const HomePage = () => {
     setFile(e.target.files[0]);
   }
 
+  const getApiLables = async () => {
+    // const client = new vision.ImageAnnotatorClient();
+    // const [result] = await client.labelDetection('https://media.istockphoto.com/id/1093508248/photo/modern-work-table-with-computer-laptop-and-cityscapes-view-from-window-business-concepts-ideas.jpg?s=612x612&w=0&k=20&c=vpMc1UR6KfgPe4GYcFG4x1FfPKLyYsoKqrAJolfBSZs=');
+    // console.log(result);
+  }
+
   useEffect(() => {
     let intreval: any;
     if (file) {
+      setLoading(true);
       intreval = setInterval(() => {
         navigate('/results', {
           state: {
             file: file
           }
         });
-      }, 1500);
+      }, 2000);
     }
     return () => {
+      setLoading(false);
       clearInterval(intreval);
     }
   },
@@ -39,6 +50,9 @@ const HomePage = () => {
       <p>האפליקציה שהופכת את מתחם העבודה שלכם לירוק וידידותי לסביבה</p>
       <InputFile onChange={(e) => handleFile(e)} text='העלה תמונה' />
       <Button onClick={() => { }} text='היסטוריית תמונות' />
+      {loading && <div className="center-body">
+        <div className="loader-circle-15"></div>
+      </div>}
     </div>
   );
 };
